c4d6769d707a4085dde144898345ace76ced0eb2
Philippe GISLARD <philippe.gislard@gmail.com>
2021-04-24 16:28:04 +0200

    Add APIs app_btplayer_i2s_config and app_byplayer_i2s_send_data

--- main/inc/app_btplayer.h
+++ main/inc/app_btplayer.h
@@ -2,5 +2,10 @@
 
 #include <stddef.h>
 
+void app_btplayer_i2s_config(
+        int sample_rate,
+        int bits_per_sample,
+        int channels);
+void app_byplayer_i2s_send_data(const uint8_t *buf, uint32_t len);
 void app_btplayer_init(const char * const mount_path, size_t mount_path_size);
 
